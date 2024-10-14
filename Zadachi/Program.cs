af9e630347890467dd200c22942f0147df028839
Gavrilchenko-Stepan <atom47228@gmail.com>
2024-10-15 00:46:43 +0300

    Сделана программа, но есть один нюанс

--- Zadachi/Program.cs
+++ Zadachi/Program.cs
@@ -10,8 +10,29 @@ namespace Zadachi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет!");
+            string[] groups = { "П-10","П-21","П-40","П-32","Ю-11","Ю-22","Ю-30","Б-10","Б-20" }; //список всех групп
+            //Console.WriteLine("Группы: " + String.Join(",", groups));
+
+            Console.WriteLine("Введите букву: ");//ввод буквы
+
+            string input = Console.ReadLine().ToString();// преобразуем введёную букву в строку
+            
+            Console.WriteLine("Группы, начинающиеся с быквы " + input + ":");
+
+            foreach (string group in groups)//перебор групп в массиве группы
+            {
+                if (group.Contains(input))
+                {
+                    Console.WriteLine(group);
+                }
+                /*else
+                {
+                  !!!
+                }
+                */
+            }
             Console.ReadKey();
+
         }
        
     }
