5091ab784096fb4282218480284283a4269038e6
Gavrilchenko-Stepan <atom47228@gmail.com>
2024-10-30 22:16:02 +0300

    Сделал более менее правильно, работает, но есть один нюанс

--- Zadachi/Program.cs
+++ Zadachi/Program.cs
@@ -11,26 +11,26 @@ namespace Zadachi
         static void Main(string[] args)
         {
             string[] groups = { "П-10","П-21","П-40","П-32","Ю-11","Ю-22","Ю-30","Б-10","Б-20" }; //список всех групп
-            //Console.WriteLine("Группы: " + String.Join(",", groups));
+            Console.WriteLine("Группы: " + String.Join(",", groups));
 
-            Console.WriteLine("Введите букву: ");//ввод буквы
-
-            string input = Console.ReadLine().ToString();// преобразуем введёную букву в строку
-            
-            Console.WriteLine("Группы, начинающиеся с быквы " + input + ":");
-
-            foreach (string group in groups)//перебор групп в массиве группы
+            Console.WriteLine("Введите номер курса: "); //ввод номера курса
+            string input = Console.ReadLine();
+            int.TryParse(input, out int result); //преобразуем строку в целое число
+            foreach (string group in groups)
             {
-                if (group.Contains(input))
+                char number = group[2];
+                if (number == (char)('0' + result)) // проверяет равенство символа number и символа полученного при сложении
                 {
                     Console.WriteLine(group);
                 }
-                /*else
+                else 
                 {
-                  !!!
+                    Console.WriteLine("Ошибка! Введите номер курас от 1 до 4!");
                 }
-                */
             }
+
+
+
             Console.ReadKey();
 
         }
