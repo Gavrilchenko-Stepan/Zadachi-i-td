ca49b2115a17d1e707202d7a1ca708779b8863db
Gavrilchenko-Stepan <atom47228@gmail.com>
2024-11-06 19:33:45 +0300

    Внёс правки

--- Zadachi/Program.cs
+++ Zadachi/Program.cs
@@ -10,7 +10,7 @@ namespace Zadachi
     {
         static void Main(string[] args)
         {
-            string[] groups = { "П-10","П-21","П-40","П-32","Ю-11","Ю-22","Ю-30","Б-10","Б-20" }; //список всех групп
+            string[] groups = {"П-10","П-21","П-40","П-32","Ю-11","Ю-22","Ю-30","Б-10","Б-20", "Ю-40", "М-41", "Б-30"}; //список всех групп
             Console.WriteLine("Группы: " + String.Join(",", groups));
 
             Console.WriteLine("Введите номер курса: "); //ввод номера курса
@@ -18,14 +18,11 @@ namespace Zadachi
             int.TryParse(input, out int result); //преобразуем строку в целое число
             foreach (string group in groups)
             {
+                
                 char number = group[2];
                 if (number == (char)('0' + result)) // проверяет равенство символа number и символа полученного при сложении
                 {
-                    Console.WriteLine(group);
-                }
-                else 
-                {
-                    Console.WriteLine("Ошибка! Введите номер курас от 1 до 4!");
+                    Console.WriteLine($"Группа {result} - го курса: {string.Join(", ", group)}");
                 }
             }
 
